81a89cf6ed5db38ba1a1df1571a82cf87546b948
pesora <machimpyo@gmail.com>
2023-10-26 16:14:04 +0900

    sec10_Polymorphism 업데이트

---
+++ source/sec10_Polymorphism and more/145_AbstracClasssC/AbstracClasssC/Cube.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstracClasssC
+{
+    internal class Cube : Shape
+    {
+        public double Length { get; set; }
+
+        public Cube(double length)
+        {
+            Name = "Cube";
+            Length = length;
+        }
+
+        public override double Volume()
+        {
+            return Math.Pow(Length, 3);
+        }
+        public override void GetInfo() 
+        {
+            base.GetInfo();
+            Console.WriteLine($"The Cube has a length of {Length}");
+        }
+    }
+}
---
+++ source/sec10_Polymorphism and more/145_AbstracClasssC/AbstracClasssC/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AbstracClasssC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = { new Sphere(4), new Cube(3) };
+            foreach(Shape shape in shapes)
+            {
+                shape.GetInfo();
+                Console.WriteLine("{0} has a volume of {1}", shape.Name, shape.Volume());
+            }
+        }
+    }
+}
