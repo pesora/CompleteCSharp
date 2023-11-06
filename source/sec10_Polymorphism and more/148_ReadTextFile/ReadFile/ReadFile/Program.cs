2801b70a43298514f9d77eb57c7100ed4c8f9006
pesora <machimpyo@gmail.com>
2023-11-06 11:59:39 +0900

    ch150까지 업데이트

---
+++ source/sec10_Polymorphism and more/148_ReadTextFile/ReadFile/ReadFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reading Text from a File
+            //exchange the adress of the file with the one you want to use
+            string text = System.IO.File.ReadAllText(@"D:\Sunshine\CSharp\CompleteCSharp\source\sec10_Polymorphism and more\148_ReadTextFile\ReadFile\textfile.txt");
+
+            Console.WriteLine("Textfile contains the following text: {0}", text);
+
+            //Receive the Text line by line.
+            string[] lines = System.IO.File.ReadAllLines(@"D:\Sunshine\CSharp\CompleteCSharp\source\sec10_Polymorphism and more\148_ReadTextFile\ReadFile\textfile.txt");
+
+            Console.WriteLine("Content of the file line by line:");
+            foreach(string line in lines)
+            {
+                //\t is a tab
+                Console.WriteLine( "\t" + line );
+            }
+
+            //To keep the Console open.
+            Console.Read();
+        }
+    }
+}
